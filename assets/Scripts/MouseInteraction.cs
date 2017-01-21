8687caf5489543142fab395fc16b4d10fbf53526
jcaonr <jcaonr@gmail.com>
2017-01-21 14:43:13 +0100

    MouseInteraction Script

---
+++ assets/Scripts/MouseInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteraction : MonoBehaviour {
+
+	public GameObject objectToPlace;
+
+	private RaycastHit hit;
+	private Vector3 hitPoint;
+
+	void Raycast()
+	{		
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+					
+		Physics.Raycast (ray, out hit);
+	}
+
+	void Update () {
+		Raycast ();
+		if(Input.GetMouseButtonDown(0))
+		{
+			PlaceObject();
+		}
+	}
+
+	void GetHitPoint(){
+		if (hit.collider != null)
+		{
+			hitPoint = hit.point;
+			Debug.Log (hitPoint);
+		} 
+	}
+
+	void PlaceObject()
+	{
+		if (hit.collider != null && hit.collider.CompareTag("EnvironmentGround"))
+		{
+			hitPoint = hit.point;
+			Debug.Log (hitPoint);
+			Instantiate (objectToPlace, hitPoint, Quaternion.identity);
+		} 
+	}
+}
