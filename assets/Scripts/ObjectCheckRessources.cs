60c8d80ca5d9101c0a7199c0a4b8fd607bd22471
jcaonr <jcaonr@gmail.com>
2017-01-22 19:44:05 +0100

    ObjectCheckRessources Script

---
+++ assets/Scripts/ObjectCheckRessources.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCheckRessources : MonoBehaviour {
+
+	public int availableRessources;
+	public List<Collider> foundColliders = new List<Collider>();
+
+	// Use this for initialization
+	void Start () {
+		float radius = gameObject.GetComponent<SphereCollider>().radius;
+
+
+		Collider[] hit;
+		hit = Physics.OverlapSphere(transform.position, radius); 
+
+		for (int i = 0; i < hit.Length; i++) {
+			Debug.Log (hit [i].name);
+		}
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		foundColliders.Add (other);
+
+		Debug.Log (foundColliders);
+		
+	}
+}
