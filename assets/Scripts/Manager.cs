7781f10221ad04a0a68cbdeb23680bfccde67cbd
Jonas Cordes <cordes@idicos.de>
2017-01-22 18:18:58 +0100

    cleanup manager grouping

--- assets/Scripts/Manager.cs
+++ assets/Scripts/Manager.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectManager : MonoBehaviour {
+public class Manager: MonoBehaviour
+{
+ public List<GameObject> population;
+ public Color color;
 
+ public void Awake ()
+ {
+  population = new List<GameObject> ();
+  gameObject.name = NameGenerator.GenRandomLastName ();
+  color = new Color (Random.Range (0f, 1.0f), Random.Range (0f, 1.0f), Random.Range (0f, 1.0f), 0.3f);
+//  self.GetComponent<Renderer> ().material.color = this.color;
+ }
+
+ public void Add (GameObject environmentObject)
+ {
+  this.population.Add (environmentObject);
+  environmentObject.GetComponent<Renderer> ().material.color = this.color;
+//  environmentObject.GetComponent<ObjectControl>().managerObject = this.gameObject;
+  environmentObject.transform.parent = this.gameObject.transform;
+
+ }
+
+ public void Merge (GameObject other)
+ {
   
+  foreach (var obj in other.GetComponent<Manager>().population) {
+   this.Add (obj);
+  }
+//
+  GameObject.Destroy (other.gameObject);
+ }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
