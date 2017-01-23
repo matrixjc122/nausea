efc7956775d04c714d1f3f62325243724ea6d809
jcaonr <jcaonr@gmail.com>
2017-01-23 18:13:16 +0100

    ObjectCheckRessources Script

--- assets/Scripts/ObjectCheckRessources.cs
+++ assets/Scripts/ObjectCheckRessources.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class ObjectCheckRessources : MonoBehaviour {
 
-	public int availableRessources;
-	public List<Collider> foundColliders = new List<Collider>();
+	public int availableRessources = 0;
 
-	// Use this for initialization
+	private List<Collider> foundColliders = new List<Collider>();
+	private RessourceProperties ressourceProperties;
+
 	void Start () {
 		float radius = gameObject.GetComponent<SphereCollider>().radius;
-
 
 		Collider[] hit;
 		hit = Physics.OverlapSphere(transform.position, radius); 
 
-		for (int i = 0; i < hit.Length; i++) {
-			Debug.Log (hit [i].name);
+		for (int i = 0; i < hit.Length; i++) 
+		{
+			if (hit[i] != null && hit[i].CompareTag("EnvironmentRessource")) {
+				RessourceProperties otherRessourceProperties = hit [i].GetComponent<RessourceProperties> ();
+				availableRessources += otherRessourceProperties.ressourceAmount;
+			}
 		}
-	}
-	
-	void OnTriggerEnter (Collider other) {
-		foundColliders.Add (other);
 
-		Debug.Log (foundColliders);
-		
+		Debug.Log ("Es befinden sich " + availableRessources + " Ressourcen im Umkreis.");
 	}
 }
