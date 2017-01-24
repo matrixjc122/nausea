2cc6597f3eb67720c10a8c9986fd79bdd347fc16
jcaonr <jcaonr@gmail.com>
2017-01-24 12:14:07 +0100

    Scene2 with RollingBall Controller

---
+++ assets/Scripts/RollingBall/BallController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float speed;
+	public float maxSpeed;
+
+	private Rigidbody rb;
+	private bool mouseOver;
+
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		mouseOver = false;
+	}	
+		
+	void FixedUpdate () 
+	{
+		if (mouseOver) {
+			float moveHorizontal = Input.GetAxis ("Mouse X");
+			float moveVertical = Input.GetAxis ("Mouse Y");
+		
+
+		Vector3 movement = new Vector3 (moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+		rb.AddForce (movement);
+		}
+		if (rb.velocity.magnitude > maxSpeed) {
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+	}
+
+	void OnMouseDrag(){
+		mouseOver = true;
+	}
+
+	void OnMouseUp(){
+		mouseOver = false;
+	}
+}
