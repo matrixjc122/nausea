61da14e396253f24fe3f49c47858a0280ff496d5
jcaonr <jcaonr@gmail.com>
2017-01-24 08:38:46 +0100

    ChangeColor of selected Object

---
+++ assets/Scripts/ObjectChangeColorOnMouseOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectChangeColorOnMouseOver : MonoBehaviour {
+
+	private Color initialColor;
+	private Renderer render;
+
+	void Start () {
+		render = GetComponent<Renderer> (); 
+		initialColor = render.material.color ;
+	}
+	
+	void OnMouseOver () {
+		render.material.color = new Color (255, 0, 0);
+	}
+
+	void OnMouseExit() {
+		render.material.color = initialColor;
+	}
+}
