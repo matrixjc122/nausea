503d11e93035cddeeb18f4b27776d0266cf83f49
jcaonr <jcaonr@gmail.com>
2017-01-25 15:20:26 +0100

    Terrain / Spawner Script

---
+++ assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public int numberOfObjects;
+	public GameObject objectToInstantiate;
+
+	private float randomX;
+	private float randomZ;
+	private Renderer r;
+
+	void Start () {
+		r = GetComponent<Renderer> ();
+
+
+		InstantiateAtPositions ();
+	}
+	
+
+	void InstantiateAtPositions () {
+		RaycastHit hit;
+
+		for ( int i = 0; i < numberOfObjects; i++){
+			randomX = Random.Range (r.bounds.min.x, r.bounds.max.x);
+			randomZ = Random.Range (r.bounds.min.z, r.bounds.max.z);
+
+
+			if (Physics.Raycast (new Vector3 (randomX, r.bounds.max.y + 5f, randomZ), -Vector3.up, out hit)) 
+			{
+				Instantiate (objectToInstantiate, hit.point, Quaternion.LookRotation(hit.normal));
+				Debug.Log (hit.normal);
+			}
+		}
+	}
+}
