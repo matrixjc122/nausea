9252cec9c1c2c4e7b6509163f68b648ddbbe1ef2
jcaonr <jcaonr@gmail.com>
2017-02-07 13:08:11 +0100

    J_SpawnManager

---
+++ assets/Scripts/Scene4/J_PlayerControls.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_PlayerControls : MonoBehaviour {
+
+	public float m_SpeedHorizontal = 10f;
+	public float m_SpeedVertical = 10f;
+
+	public Rigidbody m_Rigidbody;
+
+	private string m_MoveAxisNameHorizontal;
+	private string m_MoveAxisNameVertical;
+	private float m_MoveInputValueHorizontal;
+	private float m_MoveInputValueVertical;
+
+
+	private void OnAwake(){
+		m_Rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	private void OnEnable(){
+		m_Rigidbody.isKinematic = false;
+		m_MoveInputValueHorizontal = 0f;
+		m_MoveInputValueVertical = 0f;
+	}
+
+	private void OnDisable(){
+		m_Rigidbody.isKinematic = true;
+	}
+
+	private void Start () {
+		m_MoveAxisNameHorizontal = "Horizontal";
+		m_MoveAxisNameVertical = "Vertical";
+
+	}
+
+	private void Update () {
+		m_MoveInputValueHorizontal = Input.GetAxis (m_MoveAxisNameHorizontal);
+		m_MoveInputValueVertical = Input.GetAxis (m_MoveAxisNameVertical);
+	}
+
+	private void FixedUpdate(){
+		//Move.forward and Turn around movement
+		Move ();	
+		Turn ();
+	}
+
+	private void Move(){
+		Vector3 movementVertical = transform.forward * m_MoveInputValueVertical * m_SpeedVertical * Time.deltaTime;
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movementVertical);
+	}
+
+	private void Turn(){
+		float turn = m_MoveInputValueHorizontal * m_SpeedHorizontal * Time.deltaTime;
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+	}
+
+	private void AddForce(){
+
+		//Vector3 movement = new Vector3 (m_MoveInputValueHorizontal * m_SpeedHorizontal, 0.0f, m_MoveInputValueVertical * m_SpeedVertical);
+		//m_Rigidbody.AddForce (movement);
+
+		Vector3 movement = new Vector3 (m_MoveInputValueHorizontal  * m_SpeedHorizontal, 0.0f, m_MoveInputValueVertical * m_SpeedVertical);
+		movement = Quaternion.AngleAxis(45f, Vector3.up) * movement;
+		m_Rigidbody.AddForce (movement);
+
+	}
+}
---
+++ assets/Scripts/Scene4/J_SpawnManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_SpawnManager : MonoBehaviour {
+
+	private int rndPick;
+
+	public int numberOfObjects;
+	public GameObject[] objectToInstantiate;
+
+	private float randomX;
+	private float randomZ;
+	private Renderer r;
+
+	void Start () {
+		r = GetComponent<Renderer> ();
+
+		InstantiateAtPositions ();
+	}	
+
+	void InstantiateAtPositions () {
+		RaycastHit hit;
+
+		for ( int i = 0; i < numberOfObjects; i++){
+			randomX = Random.Range (r.bounds.min.x, r.bounds.max.x);
+			randomZ = Random.Range (r.bounds.min.z, r.bounds.max.z);
+			RandomPick ();
+
+			if (Physics.Raycast (new Vector3 (randomX, r.bounds.max.y + 5f, randomZ), -Vector3.up, out hit)) 
+			{
+				float scale = Random.Range (0.5f, 3f); 
+				float rotate = Random.Range (-270f, 270f);
+
+				GameObject obj = Instantiate (objectToInstantiate[rndPick], hit.point, Quaternion.Euler(0f, rotate, 0f));
+
+				obj.transform.localScale = new Vector3 (scale, scale, scale);  
+			}
+		}
+	}
+
+	void RandomPick(){
+		rndPick = (int)Random.Range (0f, objectToInstantiate.Length);  
+	}
+}
